8ac8a5ac0861fba4481b32cee3e74ffa8729e0ca
Cyprien <cres.cyprien@gmail.com>
2024-03-26 17:38:32 +0100

    PF-008 Ajout du CV téléchargeable

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import Footer from "./containers/footer/footer"
 import Home from "./pages/home/Home"
 import About from "./pages/about/About"
 import Projects from "./pages/projects/Projects"
-import Resume from "./pages/resume/Resume"
 import Error from "./pages/Error"
 import "./main.scss"
 
@@ -32,7 +31,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/resume" element={<Resume />} />
         <Route path="*" element={<Error />} />
       </Routes>
       <Footer />
